022c869c0169f96603ad42880a584baf3ca1e55f
Steffen Drüsedow <steffen.druesedow@telekom.de>
2016-09-28 14:56:27 +0200

    fixed mapped port number

--- docker/dockerStart.sh
+++ docker/dockerStart.sh
@@ -3,7 +3,7 @@ docker rm nomatrix
 if [ "$1" == "local" ]; then
 	LOCALPARAM="-v /home/steffen/work/git/rethink/dev-msg-node-nomatrix:/opt/volume/nomatrix --entrypoint /bin/bash "
 fi
-docker run -it --name nomatrix --net=rethink -p 2701:2701 \
+docker run -it --name nomatrix --net=rethink -p 8001:8001 \
 	-e "DOMAIN=matrix2.rethink.com" \
 	-e "PORT=8001" \
 	-e "REGISTRY=http://dev-registry-domain:4567" \
